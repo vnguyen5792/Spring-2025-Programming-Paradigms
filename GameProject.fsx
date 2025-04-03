05d25d4dceec8b1949d3b3d8db93721a624ffec5
Viet Nguyen <vietaddisonguyen@gmail.com>
2025-04-03 09:55:21 -0500

    updated game project file

--- GameProject.fsx
+++ GameProject.fsx
@@ -3,14 +3,18 @@ type Details = {Name: string; Description: string}
 
 type Item = {Details: Details; } //maybe include a # of uses? 
 
+type RoomId =
+    | RoomId of string
+
 //exit and room recursively reference each other causing an error
 //instead of type, we use the and keyword
+//but storing the same room information multiple times is inefficient. so we can make a room id type
 type Exit = 
-    | PassableExit of Details * destination: Room
+    | PassableExit of Details * destination: RoomId
     | LockedExit of Details * key: Item * next: Exit
     | NoExit of Details option
 
-and Exits = 
+type Exits = 
     {
     North: Exit
     South: Exit
@@ -18,4 +22,22 @@ and Exits =
     East: Exit
     }
 
-and Room = {Details: Details; Exits: Exits}
+type Room = 
+    {Id: RoomId
+     Details: Details
+     Items: Item list
+     Exits: Exits}
+
+type Player = 
+    {
+        Details: Details
+        Location: RoomId
+        Inventory: Item list
+    }
+
+type World = 
+    {
+        Rooms: Map<RoomId, Room>
+        Player: Player
+    }
+
